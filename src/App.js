2d3526a82cc9f461071e6e11939318f15c858ed0
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2024-07-11 13:03:50 +0200

    Create Home Screen section structure && Add & style content for left section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
 import Header from "./components/Header";
-import Home from "./components/Home";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
